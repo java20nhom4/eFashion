e6332ebeaf920121d8f7feeaeb4ed4c004986ffc
LeVinhHao <118737578+LeVinhHao@users.noreply.github.com>
2023-04-04 00:12:13 +0700

    fix script db + entity

--- eFashion_BE/src/main/java/com/cybersoft/eFashion/entity/Orders.java
+++ eFashion_BE/src/main/java/com/cybersoft/eFashion/entity/Orders.java
@@ -23,16 +23,7 @@ public class Orders {
     @JoinColumn(name = "user_id")
     private Users users;
 
-    @OneToMany(mappedBy = "orders")
-    private Set<OrderItems> listOrderItems;
 
-    public Set<OrderItems> getListOrderItems() {
-        return listOrderItems;
-    }
-
-    public void setListOrderItems(Set<OrderItems> listOrderItems) {
-        this.listOrderItems = listOrderItems;
-    }
 
     public int getId() {
         return id;
