bc9d58ddd3b0a405a0f8b905f8fbe3e15beed401
LeVinhHao <118737578+LeVinhHao@users.noreply.github.com>
2023-04-17 00:48:43 +0700

    custom popup

--- EFashion_FE/assets/js/product-details.js
+++ EFashion_FE/assets/js/product-details.js
@@ -1,16 +1,23 @@
-$(".product-details-popup").on('click', function() {
-    $(".product-details-popup-wrapper").addClass("popup")
-    $(".anywere").addClass("bgshow")
-});
-$(".product-bottom-action .view-btn").on('click', function() {
-    $(".product-details-popup-wrapper").addClass("popup")
-    $(".anywere").addClass("bgshow")
-});
-$(".product-details-popup-wrapper .cart-edit").on('click', function() {
-    $(".product-details-popup-wrapper").addClass("popup")
-    $(".anywere-home").addClass("bgshow")
-});
-$(".anywere").on('click', function() {
-    $(".product-details-popup-wrapper").removeClass("popup")
-    $(".anywere").removeClass("bgshow")
-});
+if (localStorage.getItem('token') != null) {
+    $(".addto-cart-btn").on('click', function() {
+        location.href = 'cart.html';
+    });
+
+} else {
+    $(".addto-cart-btn").on('click', function() {
+        $(".product-details-popup-wrapper").addClass("popup")
+        $(".anywere").addClass("bgshow")
+    });
+    $(".product-bottom-action .view-btn").on('click', function() {
+        $(".product-details-popup-wrapper").addClass("popup")
+        $(".anywere").addClass("bgshow")
+    });
+    $(".product-details-popup-wrapper .cart-edit").on('click', function() {
+        $(".product-details-popup-wrapper").addClass("popup")
+        $(".anywere-home").addClass("bgshow")
+    });
+    $(".anywere").on('click', function() {
+        $(".product-details-popup-wrapper").removeClass("popup")
+        $(".anywere").removeClass("bgshow")
+    });
+}
