30dfb66e70625ab4504b4f7bed0568a0cd6e17e5
Pham Ngoc Hung <phamngochung@Phams-MacBook-Pro.local>
2023-04-28 16:02:50 +0700

    [ngochung] fix cart

--- EFashion_FE/assets/js/cart.js
+++ EFashion_FE/assets/js/cart.js
@@ -1,27 +1,33 @@
-_ = document.querySelector.bind(document)
+
+_ = document.querySelector.bind(document);
 
 async function getApi(uri, token) {
-    const option = {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${ token }`,
-        },
-    }
-    const res = await fetch(uri, option);
+  const option = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  const res = await fetch(uri, option);
 
-    const data = await res.json();
+  const data = await res.json();
 
-    console.log(data.data)
-    return data.data
+  console.log(data.data);
+  return data.data;
 }
 
 async function renderData() {
-    const userId = localStorage.getItem('userId')
-    const token = localStorage.getItem('token')
-    cartItem = _('.order-item-js')
-    const data = await getApi(`http://localhost:8080/cart?userId=` + userId + ``, token);
-    cartItem.innerHTML = data.map((c) => `<tr>
+  const userId = localStorage.getItem("userId");
+  const token = localStorage.getItem("token");
+  cartItem = _(".order-item-js");
+  const data = await getApi(
+    `http://localhost:8080/cart?userId=${userId}`,
+    token
+  );
+  cartItem.innerHTML = data
+    .map(
+      (c) => `<tr>
         <td>
             <div class="product-thumb"><img style="width:130px;" src="..\\eFashion_BE\\${c.image}" alt="product-thumb"></div>
         </td>
@@ -41,83 +47,115 @@ async function renderData() {
                 </div>
             </div>
         </td>
-        <td class="last-td"><a class="remove-btn" id="${c.productId}">Remove</a></button></td>
+        <td class="last-td"><a href="#" class="remove-btn" id="${c.productId}">Remove</a></button></td>
         <td class="last-td"><a href="checkout.html?id=${c.id}" class="btn-buy" id="">Buy now</a></td>
-    </tr>`).join("")
+    </tr>`
+    )
+    .join("");
+
+  registerButtonListeners(); // register event listeners for the buttons
 }
 
-renderData()
+function registerButtonListeners() {
+  const userId = localStorage.getItem("userId");
+  const token = localStorage.getItem("token");
+  $('.remove-btn').on('click', function() {
+      const productId = $(this).attr('id')
+      const This = $(this)
 
+      var formData = new FormData();
+      formData.append('proId', productId);
+      formData.append('userId', userId);
 
-const userId = localStorage.getItem('userId')
+      $.ajax({
+          method: "DELETE",
+          headers: {
+              Authorization: `Bearer ${ token }`,
+          },
+          url: `http://localhost:8080/cart/removeProduct?proId=${productId}&userId=` + userId + ``,
+          data: formData,
+          contentType: false,
+          processData: false,
+          success: function(data) {
+              console.log('Xoá thành công !')
+          },
+          error: function() {
+              alert("Đã xảy ra lỗi khi cập nhật thông tin user");
+          }
+      }).done(function() {
+          This.closest('tr').remove()
+      })
+  });
 
-$(document).ready(function() {
-    const token = localStorage.getItem('token')
-    $('.remove-btn').on('click', function() {
-        const productId = $(this).attr('id')
-        const This = $(this)
-        $.ajax({
-            method: "DELETE",
-            headers: {
-                Authorization: `Bearer ${ token }`,
-            },
-            url: `http://localhost:8080/cart/removeProduct?proId=${productId}&userId=` + userId + ``,
-        }).done(function() {
-            This.closest('tr').remove()
-        })
-    })
-})
+  $('.btn_sub').on('click', function() {
+      const productId = $(this).attr('id')
 
-$(document).ready(function() {
-    const token = localStorage.getItem('token')
-    $('.btn_sub').on('click', function() {
-        const productId = $(this).attr('id')
-        $.ajax({
-            method: "PUT",
-            headers: {
-                Authorization: `Bearer ${ token }`,
-            },
-            url: `http://localhost:8080/cart/subtractQuantity?proId=${productId}&userId=` + userId + ``,
-        })
-    })
-})
+      var formData = new FormData();
+      formData.append('proId', productId);
+      formData.append('userId', userId);
 
-$(document).ready(function() {
-    const token = localStorage.getItem('token')
-    $('.btn_plus').on('click', function() {
-        const productId = $(this).attr('id')
+      $.ajax({
+          method: "PUT",
+          headers: {
+              Authorization: `Bearer ${ token }`,
+          },
+          url: `http://localhost:8080/cart/subtractQuantity?proId=${productId}&userId=` + userId + ``,
+          data: formData,
+          contentType: false,
+          processData: false,
+          success: function(data) {
+              console.log('this is plus !')
+          },
+          error: function() {
+              alert("Đã xảy ra lỗi khi cập nhật thông tin user");
+          }
+      })
+  });
 
-        $.ajax({
-            method: "PUT",
-            headers: {
-                Authorization: `Bearer ${ token }`,
-            },
-            url: `http://localhost:8080/cart/plusQuantity?proId=${productId}&userId=` + userId + ``,
+  $('.btn_plus').on('click', function() {
+      const productId = $(this).attr('id')
 
-        })
-    })
-})
+      var formData = new FormData();
+      formData.append('proId', productId);
+      formData.append('userId', userId);
 
-$(document).ready(function() {
-    $.ajax({
-        method: 'GET',
-        url: `http://localhost:8080/api/category/getAll`,
-    }).done(function(data) {
-        console.log(data)
-        if (data.data != null) {
-            for (const i in data.data) {
+      $.ajax({
+          method: "PUT",
+          headers: {
+              Authorization: `Bearer ${ token }`,
+          },
+          url: `http://localhost:8080/cart/plusQuantity?proId=${productId}&userId=` + userId + ``,
+          data: formData,
+          contentType: false,
+          processData: false,
+          success: function(data) {
+              console.log('this is sub !')
+          },
+          error: function() {
+              alert("Đã xảy ra lỗi khi cập nhật thông tin user");
+          }
+      })
+  });
+}
 
-                let stt = Number(Number(i) + 1)
-                const html = `<li class="mega-dropdown-li">
+renderData();
+
+$.ajax({
+  method: "GET",
+  url: `http://localhost:8080/api/category/getAll`,
+}).done(function (data) {
+  console.log(data);
+  if (data.data != null) {
+    for (const i in data.data) {
+      let stt = Number(Number(i) + 1);
+      const html = `<li class="mega-dropdown-li">
                                                 <ul class="mega-dropdown-ul">
                                                     <li class="dropdown-li"><a class="dropdown-link2"
                                                             href="products.html?cateId=${data.data[i]["id"]}">${data.data[i]["name"]}</a>
                                                     </li>                                         
                                                 </ul>
-                                </li>`
-                $('#show-cart').append(html)
-
-            }
-        }
-    })
-})
+                                </li>`;
+      $("#show-cart").append(html);
+    }
+  }
+});
